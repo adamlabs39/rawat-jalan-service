b7c795ee54279924e982e7a538d7a01792f4d2d3
Habib Fatkhul Rohman <fatkhulrohmanhabib@gmail.com>
2024-10-08 16:39:11 +0700

    add message

---
+++ src/helpers/message.js
@@ -0,0 +1,20 @@
+// message
+export const messageErrorSave = "Data gagal disimpan";
+export const messageErrorEdit = "Data gagal diedit";
+export const messageErrorShow = "Data gagal ditampilkan";
+export const messageErrorDelete = "Data gagal dihapus";
+export const messageErrorIdNotFound = "ID tidak ditemukan";
+export const messageErrorUuidNotValid = "UUID tidak valid";
+export const messageErrorDataNotFound = "Data tidak ditemukan";
+export const messageErrorMethodNotAllowed = "Metode tidak diizinkan";
+export const messageErrorConflict = "Data yang diinputkan sudah ada";
+export const messageErrorInternalServer = "Internal server error";
+export const messageErrorImport = "Data gagal di import";
+export const messageErrorExport = "Data gagal di export";
+
+export const messageSuccessSave = "Data berhasil disimpan";
+export const messageSuccessEdit = "Data berhasil diedit";
+export const messageSuccessShow = "Data berhasil ditampilkan";
+export const messageSuccessDelete = "Data berhasil dihapus";
+export const messageSuccessImport = "Data berhasil di import";
+export const messageSuccessExport = "Data berhasil di export";
