a18b88e39042ff7a139707ad5e160f33e646bbac
MahendraKhibrah <mahendra.khibrah@socialbread.id>
2024-11-08 14:12:14 +0700

    integrate model & auth sdk

--- src/app.js
+++ src/app.js
@@ -3,8 +3,7 @@ import "dotenv/config";
 import cors from "cors";
 import routes from "./routes/routes.js";
 import errorMiddleware from "./middlewares/error-middleware.js";
-import syncDB from "./models/model-synchronize.js";
-import authorizationSdk from "authorization-sdk";
+import authorizationSdk from "@adameds/authorization-sdk";
 
 const APPLICATION_PORT = process.env.APPLICATION_PORT;
 const APPLICATION_HOST = process.env.APPLICATION_HOST;
@@ -12,7 +11,6 @@ const API_BASE = process.env.API_BASE || "api";
 const API_VERSION = process.env.API_VERSION || "v3";
 const API_MODULE = process.env.API_MODULE || "pelayanan";
 const BASE_URL = `/${API_BASE}/${API_VERSION}/${API_MODULE}`;
-syncDB();
 
 const app = express();
 app.use(
@@ -30,7 +28,6 @@ app.use(
   })
 );
 
-// app.use(authorizationMiddleware);
 app.use(authorizationSdk([]));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
