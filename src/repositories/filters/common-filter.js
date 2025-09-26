02bf17b7df48cce5aeba53a8cefc592220c9a56b
Raihan <raihanm854@gmail.com>
2025-09-26 09:37:29 +0700

    adjust: filter sesuaikan dengan fe

--- src/repositories/filters/common-filter.js
+++ src/repositories/filters/common-filter.js
@@ -17,7 +17,7 @@ export function commonFilterReport({ faskesUuid, args = {}, options = {} }) {
         ...options
     }
 
-    if (args.dokter) filter.practitionerUuid = args.dokter;
+    if (args.practitioner_uuid) filter.practitionerUuid = args.practitioner_uuid;
 
     return filter;
 }
